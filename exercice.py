d15df0e829bc3a0d17137abe93ae19afcc580289
SamimNiazi <samim.niazi@polymtl.ca>
2021-10-20 15:40:43 -0400

    commencement

--- exercice.py
+++ exercice.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
+import fonction1
 
 
 # TODO: Définissez vos fonction ici
@@ -10,5 +10,5 @@
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
+    fonction1.volume(a=2,b=4,c=6, masse_volumique=10)
     pass
